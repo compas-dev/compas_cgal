933a4a9fea9499a924a7498c2f85ebc5e9d983b9
Tom Van Mele <vanmelet@ethz.ch>
2020-05-17 22:18:37 +0200

    compas header

---
+++ include/compas.hpp
@@ -0,0 +1,4 @@
+#include <pybind11/eigen.h>
+
+using RowMatrixXd = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+using RowMatrixXi = Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
--- include/compas_cgal.hpp
+++ include/compas_cgal.hpp
@@ -1,4 +1,4 @@
-#include <pybind11/eigen.h>
+#include <compas.hpp>
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/Surface_mesh.h>
 
@@ -8,9 +8,6 @@ using Point3 = Kernel::Point_3;
 using Mesh = CGAL::Surface_mesh<Point3>;
 
 using vertex_descriptor = Mesh::Vertex_index;
-
-using RowMatrixXd = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
-using RowMatrixXi = Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
 
 
 Mesh mesh_from_vertices_and_faces(RowMatrixXd V, RowMatrixXi F);
