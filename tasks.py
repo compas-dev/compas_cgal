fcb017c84e934f8b38f89a53664888f515253a42
Li <leochen1990@gmail.com>
2021-05-05 15:31:33 +0200

    update workflow and tasks

--- tasks.py
+++ tasks.py
@@ -1,8 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import contextlib
+import glob
 import os
 import sys
 from shutil import rmtree
 
-from invoke import Exit, task
+from invoke import Exit
+from invoke import task
 
 try:
     input = raw_input
@@ -40,10 +46,13 @@ log = Log()
 def confirm(question):
     while True:
         response = input(question).lower().strip()
+
         if not response or response in ('n', 'no'):
             return False
+
         if response in ('y', 'yes'):
             return True
+
         print('Focus, kid! It is either (y)es or (n)o', file=sys.stderr)
 
 
@@ -57,54 +66,62 @@ def help(ctx):
 @task(help={
     'docs': 'True to clean up generated documentation, otherwise False',
     'bytecode': 'True to clean up compiled python files, otherwise False.',
-    'builds': 'True to clean up build/packaging artifacts, otherwise False.'
-})
+    'builds': 'True to clean up build/packaging artifacts, otherwise False.'})
 def clean(ctx, docs=True, bytecode=True, builds=True):
     """Cleans the local copy from compiled artifacts."""
-    if builds:
-        ctx.run('python setup.py clean')
-    if bytecode:
-        for root, dirs, files in os.walk(BASE_FOLDER):
-            for f in files:
-                if f.endswith('.pyc'):
-                    os.remove(os.path.join(root, f))
-            if '.git' in dirs:
-                dirs.remove('.git')
-    folders = []
-    folders.append('dist/')
-    if docs:
-        folders.append('docs/api/generated')
-    if bytecode:
-        folders.append('src/compas_cgal/__pycache__')
-    if builds:
-        folders.append('build/')
-        folders.append('src/compas_cgal.egg-info/')
-    for folder in folders:
-        rmtree(os.path.join(BASE_FOLDER, folder), ignore_errors=True)
+
+    with chdir(BASE_FOLDER):
+        if builds:
+            ctx.run('python setup.py clean')
+
+        if bytecode:
+            for root, dirs, files in os.walk(BASE_FOLDER):
+                for f in files:
+                    if f.endswith('.pyc'):
+                        os.remove(os.path.join(root, f))
+                if '.git' in dirs:
+                    dirs.remove('.git')
+
+        folders = []
+
+        if docs:
+            folders.append('docs/api/generated')
+
+        folders.append('dist/')
+
+        if bytecode:
+            for t in ('src', 'tests'):
+                folders.extend(glob.glob('{}/**/__pycache__'.format(t), recursive=True))
+
+        if builds:
+            folders.append('build/')
+            folders.append('src/compas_cgal.egg-info/')
+
+        for folder in folders:
+            rmtree(os.path.join(BASE_FOLDER, folder), ignore_errors=True)
 
 
 @task(help={
-    'rebuild': 'True to clean all previously built docs before starting, otherwise False.',
-    'doctest': 'True to run doctests, otherwise False.',
-    'check_links': 'True to check all web links in docs for validity, otherwise False.'})
-def docs(ctx, doctest=False, rebuild=True, check_links=False):
+      'rebuild': 'True to clean all previously built docs before starting, otherwise False.',
+      'doctest': 'True to run doctests, otherwise False.',
+      'check_links': 'True to check all web links in docs for validity, otherwise False.'})
+def docs(ctx, doctest=False, rebuild=False, check_links=False):
     """Builds package's HTML documentation."""
+
     if rebuild:
         clean(ctx)
-    if doctest:
-        ctx.run('sphinx-build -b doctest docs dist/docs')
-    ctx.run('sphinx-build -E docs dist/docs')
-    if check_links:
-        ctx.run('sphinx-build -b linkcheck docs dist/docs')
 
+    with chdir(BASE_FOLDER):
+        # ctx.run('sphinx-autogen docs/**.rst')
 
-@task()
-def check(ctx):
-    """Check the consistency of documentation, coding style and a few other things."""
-    log.write('Checking MANIFEST.in...')
-    ctx.run('check-manifest')
-    log.write('Checking metadata...')
-    ctx.run('python setup.py check --strict --metadata')
+        if doctest:
+            testdocs(ctx, rebuild=rebuild)
+
+        opts = '-E' if rebuild else ''
+        ctx.run('sphinx-build {} -b html docs dist/docs'.format(opts))
+
+        if check_links:
+            linkcheck(ctx, rebuild=rebuild)
 
 
 @task()
@@ -114,23 +131,96 @@ def lint(ctx):
     ctx.run('flake8 src')
 
 
+@task()
+def testdocs(ctx, rebuild=False):
+    """Test the examples in the docstrings."""
+    log.write('Running doctest...')
+    opts = '-E' if rebuild else ''
+    ctx.run('sphinx-build {} -b doctest docs dist/docs'.format(opts))
+
+
+@task()
+def linkcheck(ctx, rebuild=False):
+    """Check links in documentation."""
+    log.write('Running link check...')
+    opts = '-E' if rebuild else ''
+    ctx.run('sphinx-build {} -b linkcheck docs dist/docs'.format(opts))
+
+
+@task()
+def check(ctx):
+    """Check the consistency of documentation, coding style and a few other things."""
+
+    with chdir(BASE_FOLDER):
+        lint(ctx)
+
+        log.write('Checking MANIFEST.in...')
+        ctx.run('check-manifest')
+
+        log.write('Checking metadata...')
+        ctx.run('python setup.py check --strict --metadata')
+
+
 @task(help={
-    'checks': 'True to run all checks before testing, otherwise False.'})
+      'checks': 'True to run all checks before testing, otherwise False.'})
 def test(ctx, checks=False, doctest=False):
     """Run all tests."""
     if checks:
         check(ctx)
-    cmd = ['pytest']
-    if doctest:
-        cmd.append('--doctest-modules')
-    ctx.run(' '.join(cmd))
+
+    with chdir(BASE_FOLDER):
+        cmd = ['pytest']
+        if doctest:
+            cmd.append('--doctest-modules')
+
+        ctx.run(' '.join(cmd))
+
+
+@task
+def prepare_changelog(ctx):
+    """Prepare changelog for next release."""
+    UNRELEASED_CHANGELOG_TEMPLATE = '## Unreleased\n\n### Added\n\n### Changed\n\n### Removed\n\n\n## '
+
+    with chdir(BASE_FOLDER):
+        # Preparing changelog for next release
+        with open('CHANGELOG.md', 'r+') as changelog:
+            content = changelog.read()
+            changelog.seek(0)
+            changelog.write(content.replace(
+                '## ', UNRELEASED_CHANGELOG_TEMPLATE, 1))
+
+        ctx.run('git add CHANGELOG.md && git commit -m "Prepare changelog for next release"')
 
 
 @task(help={
-    'version_type': 'Type of version follows semver rules. Must be one of: major, minor, patch.'})
-def bumpversion(ctx, version_type):
-    """Bump the version"""
-    if version_type not in ('patch', 'minor', 'major'):
-        raise Exit('The version type parameter is invalid.\nMust be one of: major, minor, patch')
+      'release_type': 'Type of release follows semver rules. Must be one of: major, minor, patch, major-rc, minor-rc, patch-rc, rc, release.'})
+def release(ctx, release_type):
+    """Releases the project in one swift command!"""
+    if release_type not in ('patch', 'minor', 'major', 'major-rc', 'minor-rc', 'patch-rc', 'rc', 'release'):
+        raise Exit('The release type parameter is invalid.\nMust be one of: major, minor, patch, major-rc, minor-rc, patch-rc, rc, release')
+
+    is_rc = release_type.find('rc') >= 0
+    release_type = release_type.split('-')[0]
+
+    # Run checks
+    ctx.run('invoke check')
+
     # Bump version and git tag it
-    ctx.run('bumpversion %s --verbose' % version_type)
+    if is_rc:
+        ctx.run('bump2version %s --verbose' % release_type)
+    elif release_type == 'release':
+        ctx.run('bump2version release --verbose')
+    else:
+        ctx.run('bump2version %s --verbose --no-tag' % release_type)
+        ctx.run('bump2version release --verbose')
+
+
+@contextlib.contextmanager
+def chdir(dirname=None):
+    current_dir = os.getcwd()
+    try:
+        if dirname is not None:
+            os.chdir(dirname)
+        yield
+    finally:
+        os.chdir(current_dir)
