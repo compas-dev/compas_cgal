921f4efca8680375ae96fab9d67732e968919f86
brgcode <vanmelet@ethz.ch>
2021-06-07 14:30:42 +0200

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -147,7 +147,7 @@ class BuildExt(build_ext):
 
 setup(
     name="compas_cgal",
-    version="0.1.2rc0",
+    version="0.1.2",
     description="COMPAS friedly bindings for the CGAL library.",
     long_description=long_description,
     long_description_content_type="text/markdown",
