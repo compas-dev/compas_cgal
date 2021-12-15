cd13c81025bc5b225070829d68bba6c39785826c
brgcode <van.mele@arch.ethz.ch>
2021-12-15 16:09:21 +0100

    make triangulation available as plugin

--- src/compas_cgal/triangulation.py
+++ src/compas_cgal/triangulation.py
@@ -1,14 +1,16 @@
 import numpy as np
+from compas.plugins import plugin
 from compas_cgal._cgal import triangulations
 
 
-def delaunay_triangulation(vertices):
-    """Construct a Delaunay triangulation.
+@plugin(category="traingulation", requires=['compas_cgal'])
+def delaunay_triangulation(points):
+    """Construct a Delaunay triangulation from a set of points.
 
     Parameters
     ----------
-    vertices : list of :class:`compas.geometry.Point`
-        Vertices of the triangulation.
+    points : list of :class:`compas.geometry.Point`
+        Points of the triangulation.
 
     Returns
     -------
@@ -20,10 +22,11 @@ def delaunay_triangulation(vertices):
     ...
 
     """
-    vertices = np.asarray(vertices, dtype=np.float64)
+    vertices = np.asarray(points, dtype=np.float64)
     return triangulations.delaunay_triangulation(vertices)
 
 
+@plugin(category='triangulation', requires=['compas_cgal'])
 def constrained_delaunay_triangulation(boundary, points=None, holes=None, curves=None):
     """Construct a Constrained Delaunay triangulation.
 
@@ -66,6 +69,7 @@ def constrained_delaunay_triangulation(boundary, points=None, holes=None, curves
     return triangulations.constrained_delaunay_triangulation(boundary, points, holes, curves, is_conforming=False)
 
 
+@plugin(category='triangulation', requires=['compas_cgal'])
 def conforming_delaunay_triangulation(boundary, points=None, holes=None, curves=None):
     """Construct a Conforming Delaunay triangulation.
 
@@ -109,7 +113,7 @@ def conforming_delaunay_triangulation(boundary, points=None, holes=None, curves=
 
 
 def refined_delaunay_mesh(boundary, points=None, holes=None, curves=None, maxlength=None, is_optimized=False):
-    """Construct a refined Delaunay mesh.
+    """Construct a refined Delaunay mesh [1]_.
 
     Parameters
     ----------
@@ -131,9 +135,9 @@ def refined_delaunay_mesh(boundary, points=None, holes=None, curves=None, maxlen
     list
         The vertices and faces of the triangulation.
 
-    Notes
-    -----
-    ...
+    References
+    ----------
+    .. [1] https://doc.cgal.org/latest/Mesh_2/index.html#secMesh_2_meshes
 
     """
     boundary = np.asarray(boundary, dtype=np.float64)
