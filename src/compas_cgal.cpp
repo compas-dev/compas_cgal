9f7136f3fe7400c6b5707d3d1772a55800b198eb
brgcode <van.mele@arch.ethz.ch>
2022-01-20 09:49:35 +0100

    comments

--- src/compas_cgal.cpp
+++ src/compas_cgal.cpp
@@ -11,6 +11,8 @@ void init_measure(py::module&);
 void init_triangulations(py::module&);
 void init_subdivision(py::module&);
 
+// the first parameter here ("_cgal") will be the name of the "so" or "pyd" file that will be produced by PyBind11
+// which is the entry point from where all other modules will be accessible.
 PYBIND11_MODULE(_cgal, m) {
     m.doc() = "";
 
@@ -18,6 +20,7 @@ PYBIND11_MODULE(_cgal, m) {
     	.def_readonly("vertices", &compas::Result::vertices)
     	.def_readonly("faces", &compas::Result::faces);
 
+    // here all modules of "_cgal" are initializied.
     init_meshing(m);
     init_booleans(m);
     init_slicer(m);
