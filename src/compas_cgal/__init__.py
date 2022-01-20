b6bcd1abbf2da185550e17b7f2fca5d870651a20
brgcode <van.mele@arch.ethz.ch>
2022-01-20 12:48:22 +0100

    Bump version to 0.4.0

--- src/compas_cgal/__init__.py
+++ src/compas_cgal/__init__.py
@@ -83,7 +83,7 @@ __author__ = ["tom van mele"]
 __copyright__ = "Block Research Group - ETH Zurich"
 __license__ = "MIT License"
 __email__ = "van.mele@arch.ethz.ch"
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 HERE = os.path.dirname(__file__)
 
