5479bc617f6f98d9e7d275b5c9c160143eba35e5
tomvanmele <tom.v.mele@gmail.com>
2024-02-02 00:05:01 +0100

    add the hash script

---
+++ recipe/sha256.py
@@ -0,0 +1,10 @@
+import hashlib
+from pathlib import Path
+
+FILE = Path(__file__).parent / "compas_cgal-0.6.0.tar.gz"
+
+with open(FILE, "rb") as f:
+    data = f.read()
+    h = hashlib.sha256(data).hexdigest()
+
+print(h)
