ad78cd0a12d20b53305aedfd59f030d17b58c57c
Romana Rust <romana.rust@gmail.com>
2024-09-18 10:17:34 +0200

    updating types

--- src/compas_cgal/straight_skeleton_2.py
+++ src/compas_cgal/straight_skeleton_2.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+from typing import Union
+
 import numpy as np
 from compas.datastructures import Graph
 from compas.geometry import Polygon
@@ -6,19 +9,23 @@ from compas.tolerance import TOL
 
 from compas_cgal._cgal import straight_skeleton_2
 
+from .types import IntNx1
+from .types import IntNx2
+from .types import VerticesNumpy
 
-def graph_from_skeleton_data(points, indices, edges, edge_types) -> Graph:
+
+def graph_from_skeleton_data(points: VerticesNumpy, indices: IntNx1, edges: IntNx2, edge_types: IntNx1) -> Graph:
     """Create a graph from the skeleton data.
 
     Parameters
     ----------
-    points : list of point coordinates
-        The vertices of the skeleton.
-    indices : list of int
+    points : :class:`numpy.ndarray`
+        The vertices of the skeleton, each vertex defined by 3 spatial coordinates.
+    indices : :class:`numpy.ndarray`
         The vertex indices of the skeleton, corresponding to the points.
-    edges : list of tuple of int
-        The edges of the skeleton.
-    edge_types : list of int
+    edges : :class:`numpy.ndarray`
+        The edges of the skeleton, each edge defined by 2 vertex indices.
+    edge_types : :class:`numpy.ndarray`
         The type per edge, `0` for inner bisector, `1` for bisector, and `2` for boundary.
 
     Returns
@@ -41,13 +48,15 @@ def graph_from_skeleton_data(points, indices, edges, edge_types) -> Graph:
     return graph
 
 
-def create_interior_straight_skeleton(points) -> Graph:
+def create_interior_straight_skeleton(points, as_graph=True) -> Union[Graph, Tuple[VerticesNumpy, IntNx1, IntNx2, IntNx1]]:
     """Compute the skeleton of a polygon.
 
     Parameters
     ----------
     points : list of point coordinates or :class:`compas.geometry.Polygon`
         The points of the polygon.
+    as_graph : bool, optional
+        Whether the skeleton should be returned as a graph, defaults to `True`.
 
     Returns
     -------
@@ -64,10 +73,13 @@ def create_interior_straight_skeleton(points) -> Graph:
     if not TOL.is_allclose(normal, [0, 0, 1]):
         raise ValueError("The normal of the polygon should be [0, 0, 1]. The normal of the provided polygon is {}".format(normal))
     V = np.asarray(points, dtype=np.float64)
-    return graph_from_skeleton_data(*straight_skeleton_2.create_interior_straight_skeleton(V))
+    points, indices, edges, edge_types = straight_skeleton_2.create_interior_straight_skeleton(V)
+    if as_graph:
+        return graph_from_skeleton_data(points, indices, edges, edge_types)
+    return points, indices, edges, edge_types
 
 
-def create_interior_straight_skeleton_with_holes(points, holes) -> Graph:
+def create_interior_straight_skeleton_with_holes(points, holes, as_graph=True) -> Union[Graph, Tuple[VerticesNumpy, IntNx1, IntNx2, IntNx1]]:
     """Compute the skeleton of a polygon with holes.
 
     Parameters
@@ -76,6 +88,8 @@ def create_interior_straight_skeleton_with_holes(points, holes) -> Graph:
         The points of the polygon.
     holes : list of list of point coordinates or list of :class:`compas.geometry.Polygon`
         The holes of the polygon.
+    as_graph : bool, optional
+        Whether the skeleton should be returned as a graph, defaults to `True`.
 
     Returns
     -------
@@ -102,7 +116,10 @@ def create_interior_straight_skeleton_with_holes(points, holes) -> Graph:
             raise ValueError("The normal of the hole should be [0, 0, -1]. The normal of the provided {}-th hole is {}".format(i, normal_hole))
         hole = np.asarray(points, dtype=np.float64)
         H.append(hole)
-    return graph_from_skeleton_data(*straight_skeleton_2.create_interior_straight_skeleton_with_holes(V, H))
+    points, indices, edges, edge_types = straight_skeleton_2.create_interior_straight_skeleton_with_holes(V, H)
+    if as_graph:
+        return graph_from_skeleton_data(points, indices, edges, edge_types)
+    return points, indices, edges, edge_types
 
 
 def create_offset_polygons_2(points, offset) -> list[Polygon]:
