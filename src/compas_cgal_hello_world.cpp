644cc41140ef0d4537a6bd6fedf4bc2aead94f70
Petras Vestartas <petrasvestartas@gmail.com>
2025-02-28 11:58:41 +0100

    MILESTONE boost multiprecision is linked with compas_cgal headers, wrapping starts.

--- src/compas_cgal_hello_world.cpp
+++ src/compas_cgal_hello_world.cpp
@@ -1,9 +1,3 @@
-#include <nanobind/nanobind.h>
-
-namespace nb = nanobind;
-
-using namespace nb::literals;
-
 NB_MODULE(compas_cgal_ext, m) {
     m.doc() = "Nanobind says hello to COMPAS!";
     m.def("add", [](int a, int b) { return a + b; }, "a"_a, "b"_a);
