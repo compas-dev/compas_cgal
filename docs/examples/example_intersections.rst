05144e7d15fc5c16f91abe426bcfdf8426f6a01b
Petras Vestartas <petrasvestartas@gmail.com>
2025-03-20 10:40:48 +0100

    ADD intersections

---
+++ docs/examples/example_intersections.rst
@@ -0,0 +1,17 @@
+Mesh Intersections
+=====================
+
+This example demonstrates how to compute intersections between two triangle meshes using COMPAS CGAL.
+
+Key Features:
+
+* Computing intersection curves between meshes
+* Visualizing intersection results
+* Handling multiple intersection curves
+
+.. figure:: /_images/example_intersections.png
+    :figclass: figure
+    :class: figure-img img-fluid
+
+.. literalinclude:: example_intersections.py
+   :language: python
---
+++ src/intersections.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "compas.h"
+
+// CGAL intersection
+#include <CGAL/Polygon_mesh_processing/intersection.h>
+
+/**
+ * Compute intersection between two triangle meshes.
+ *
+ * @param vertices_a Vertices of mesh A as Nx3 matrix in row-major order (float64)
+ * @param faces_a Faces of mesh A as Mx3 matrix of vertex indices in row-major order (int32)
+ * @param vertices_b Vertices of mesh B as Px3 matrix in row-major order (float64)
+ * @param faces_b Faces of mesh B as Qx3 matrix of vertex indices in row-major order (int32)
+ * @return std::vector<RowMatrixXd> containing:
+ *         - List of intersection polylines, each as Rx3 matrix of points (float64)
+ * @note Input meshes must be manifold and closed
+ */
+std::vector<compas::RowMatrixXd>
+pmp_intersection_mesh_mesh(
+    Eigen::Ref<const compas::RowMatrixXd> vertices_a,
+    Eigen::Ref<const compas::RowMatrixXi> faces_a,
+    Eigen::Ref<const compas::RowMatrixXd> vertices_b,
+    Eigen::Ref<const compas::RowMatrixXi> faces_b);
