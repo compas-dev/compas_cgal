bcdc3fce1b3f1d7c6c6e7bc3ecfe5d2f88dfc3c5
Petras Vestartas <petrasvestartas@gmail.com>
2025-06-24 18:17:00 +0200

    REMOVE unnecessary function declaration.

--- docs/examples/example_projection.py
+++ docs/examples/example_projection.py
@@ -6,22 +6,15 @@ from compas_viewer import Viewer
 from compas_cgal.meshing import mesh_project
 
 
-def main():
-    """Project a mesh to another mesh."""
+# Mesh to project
+input_file_0 = Path(__file__).parent.parent.parent / "data" / "rhinovault_mesh_0.ply"
+mesh_0 = Mesh.from_ply(input_file_0)
 
-    # Mesh to project
-    input_file_0 = Path(__file__).parent.parent.parent / "data" / "rhinovault_mesh_0.ply"
-    mesh_0 = Mesh.from_ply(input_file_0)
+# Target Mesh to project onto
+input_file_1 = Path(__file__).parent.parent.parent / "data" / "rhinovault_mesh_1.ply"
+mesh_1 = Mesh.from_ply(input_file_1)
 
-    # Target Mesh to project onto
-    input_file_1 = Path(__file__).parent.parent.parent / "data" / "rhinovault_mesh_1.ply"
-    mesh_1 = Mesh.from_ply(input_file_1)
-
-    mesh_result = mesh_project(mesh_0, mesh_1, use_normals=False)
-    return mesh_0, mesh_1, mesh_result
-
-
-mesh_0, mesh_1, mesh_result = main()
+mesh_result = mesh_project(mesh_0, mesh_1, use_normals=False)
 
 # ==============================================================================
 # Visualize
